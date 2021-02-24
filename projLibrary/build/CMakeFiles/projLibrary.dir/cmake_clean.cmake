ff4abbd0d302def7ae29eb2e00de6f781867d36c
Asama Al-Ghafri <fy19akhm@feng-linux-03.leeds.ac.uk>
2021-02-24 18:43:35 +0000

    update in bulding the proj

---
+++ projLibrary/build/CMakeFiles/projLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/projLibrary.dir/main.c.o"
+  "CMakeFiles/projLibrary.dir/Users.c.o"
+  "CMakeFiles/projLibrary.dir/book_management.c.o"
+  "projLibrary.pdb"
+  "projLibrary"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/projLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
