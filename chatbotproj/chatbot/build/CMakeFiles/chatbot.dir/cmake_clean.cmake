c13eb35730a5371fa67dd0e8aae1e7f9d3dfcd2d
Asama Al-Ghafri <fy19akhm@feng-linux-06.leeds.ac.uk>
2021-04-19 07:44:35 +0100

    successful bulding

---
+++ chatbotproj/chatbot/build/CMakeFiles/chatbot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/chatbot.dir/main.c.o"
+  "chatbot.pdb"
+  "chatbot"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/chatbot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
